91c90cab3b8c206d9c53137df394f5273d7250ad
AvtoG <agrigolia@cca.edu>
2017-01-29 00:02:18 -0800

    Create sketch-4.js

---
+++ sketch-4.js
@@ -0,0 +1,16 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+var a=100
+var b= 100
+var c=3
+ 
+function draw() { 
+	background(255)
+	
+	ellipse(a,b,40,40)
+	
+	if(a>400) {
+		c=-3
+} a=a+c
+}
